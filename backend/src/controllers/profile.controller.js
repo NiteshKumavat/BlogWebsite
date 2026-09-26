a0b80109a592c7a93a376b05bc07a9dd279463a1
Nitesh Kumavat <niteshkumavat32@gmail.com>
2026-09-26 21:00:57 +0530

    defining the controllers

---
+++ backend/src/controllers/profile.controller.js
@@ -0,0 +1,7 @@
+export const createProfile = async(req, res) => {};
+export const getProfile = async(req, res) => {};
+export const updateProfile = async(req, res) => {};
+export const deleteProfile = async(req, res) => {};
+export const getAllProfiles = async(req, res) => {};
+
+
